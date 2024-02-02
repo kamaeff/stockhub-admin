a2c10b50d08198533dab5c965496b476820a2b81
kamaeff <kamaeff2@gmail.com>
2024-02-02 14:13:19 +0300

    add icon

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include_once("connect.php");
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="image/x-icon" href="./assets/icon/icon.ico">
 	<title>AdminPanel</title>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
